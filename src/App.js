8e5be5f11c01f19e44bbc7ff51b1abfb023ca6d8
Sanket <sanket.anvekar10.sa.sa@gmail.com>
2024-11-27 14:34:52 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alerts from './components/Alerts';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      message: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#12273d';
+      showAlert("Dark Mode Enables", "success");
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode Enables", "success");
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+       <Navbar title = "TextUtils"  mode={mode} toggleMode={toggleMode}  />
+        <Alerts alert={alert} />
+        <div className='container my-3'>
+          {/* <Routes> */}
+            {/* <Route path="/" element={<TextForm showAlert={showAlert} heading="Textutils- Word Counter & Character Counter" mode={mode}/>} /> */}
+            {/* <Route path="/about" element={<About mode={mode}/>} />
+          </Routes> */}
+          <TextForm showAlert={showAlert} heading="Textutils- Word Counter & Character Counter" mode={mode}/>
+        </div>
+      {/* </Router> */}
+    </>
   );
 }
+
 
 export default App;
